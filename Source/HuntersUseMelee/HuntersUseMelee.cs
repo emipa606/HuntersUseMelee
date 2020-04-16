bf2afd539ea2902ae71fe2f15b45b4956e990e17
Kristian Schlikow <kristian@schlikow.de>
2020-04-16 06:31:57 +0200

    Upload source code

---
+++ Source/HuntersUseMelee/HuntersUseMelee.cs
@@ -0,0 +1,41 @@
+﻿// This project is subject to the terms of the Mozilla Public License v2.0
+// If a copy of the MPL was not distributed with this file,
+// You can obtain one at https://mozilla.org/MPL/2.0/
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace HuntersUseMelee
+{    
+    [StaticConstructorOnStartup]
+    static class HarmonyPatches
+    {
+        static HarmonyPatches()
+        {
+            var harmony = new Harmony("net.netrve.huntersusemelee");
+
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(WorkGiver_HunterHunt), "HasHuntingWeapon")]
+        internal static class Patch_HasHuntingWeapon
+        { 
+            static void Postfix(Pawn p, ref bool __result)
+            {
+                if (!__result)
+                    __result = p.equipment.Primary != null && p.equipment.Primary.def.IsMeleeWeapon && (p.equipment.PrimaryEq.PrimaryVerb.HarmsHealth());
+            }
+        }
+    
+        [HarmonyPatch(typeof(Alert_HunterLacksRangedWeapon))]
+        [HarmonyPatch(MethodType.Constructor)]
+        internal static class Patch_HuntersLacksWeaponAlert
+        { 
+            static void Postfix(ref string ___defaultLabel, ref string ___defaultExplanation)
+            {
+                ___defaultLabel = "HUMHunterLacksWeapon".Translate();
+                ___defaultExplanation = "HUMHunterLacksWeaponDesc".Translate();
+            }
+        }
+    }
+}
---
+++ Source/HuntersUseMelee/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HuntersUseMelee")]
+[assembly: AssemblyDescription("Allows hunters to make use of melee weapons to do their job.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Netrve")]
+[assembly: AssemblyProduct("HuntersUseMelee")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("DF3F3E2F-A70B-47E6-821C-A36EC8D96DD4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
