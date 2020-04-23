16b55218ac9ede45c9b5dfb63b43e32ccf130ca7
Kristian Schlikow <kristian@schlikow.de>
2020-04-23 17:57:49 +0200

    Added support for using fists and simple sidearms
    Includes a mod settings menu as well

--- Source/HuntersUseMelee/HuntersUseMelee.cs
+++ Source/HuntersUseMelee/HuntersUseMelee.cs
@@ -1,12 +1,33 @@
 ﻿// This project is subject to the terms of the Mozilla Public License v2.0
 // If a copy of the MPL was not distributed with this file,
 // You can obtain one at https://mozilla.org/MPL/2.0/
+
+using System.Linq;
+using SimpleSidearms;
 using HarmonyLib;
 using RimWorld;
 using Verse;
 
 namespace HuntersUseMelee
-{    
+{
+    [StaticConstructorOnStartup]
+    public static class HuntersUseMeleeMain
+    {
+        public static readonly bool SimpleSidearmsLoaded;
+
+        // Constructor just used to see if Simple Sidearms is loaded
+        static HuntersUseMeleeMain()
+        {
+            SimpleSidearmsLoaded = IsLoaded("PeteTimesSix.SimpleSidearms");
+        }
+
+        // Utility function to check if a mod is loaded based on its packageId
+        private static bool IsLoaded(string packageId)
+        {
+            return LoadedModManager.RunningModsListForReading.Any(x => x.PackageId == packageId);
+        }
+    }
+
     [StaticConstructorOnStartup]
     static class HarmonyPatches
     {
@@ -14,27 +35,52 @@ namespace HuntersUseMelee
         {
             var harmony = new Harmony("net.netrve.huntersusemelee");
 
+            // We patch all as we use annotations
             harmony.PatchAll();
         }
 
+        // We patch the WorkGiver for Hunting, more specifically 
+        // The check used to see if a pawn has a hunting weapon
         [HarmonyPatch(typeof(WorkGiver_HunterHunt), "HasHuntingWeapon")]
         internal static class Patch_HasHuntingWeapon
-        { 
+        {
             static void Postfix(Pawn p, ref bool __result)
             {
-                if (!__result)
-                    __result = p.equipment.Primary != null && p.equipment.Primary.def.IsMeleeWeapon && (p.equipment.PrimaryEq.PrimaryVerb.HarmsHealth());
+                // No need to run if the result already is true
+                if (__result) return;
+
+                // If Fist Fighting is enabled, anything goes so we return true
+                if (HuntersUseMeleeMod.settings.enableFistFighting)
+                {
+                    __result = true;
+                    return;
+                }
+                
+                // Check if primary is a valid, damaging melee weapon
+                // Automatically assign the result
+                __result = p.equipment.Primary != null && p.equipment.Primary.def.IsMeleeWeapon &&
+                           p.equipment.PrimaryEq.PrimaryVerb.HarmsHealth();
+
+                // Simple Sidearms support
+                // As above, we skip if result is already true, but we also skip is setting is off
+                // Or Simple Sidearms isn't loaded
+                if (__result || !(HuntersUseMeleeMain.SimpleSidearmsLoaded && HuntersUseMeleeMod.settings.enableSimpleSidearms)) return;
+                
+                // If the pawn can carry sidearms and has any, they are good to go
+                if (p.getCarriedWeapons().Any())
+                    __result = true;
             }
-        }
-    
-        [HarmonyPatch(typeof(Alert_HunterLacksRangedWeapon))]
-        [HarmonyPatch(MethodType.Constructor)]
-        internal static class Patch_HuntersLacksWeaponAlert
-        { 
-            static void Postfix(ref string ___defaultLabel, ref string ___defaultExplanation)
+
+            // No need to redo the Alert, we simply change the text to fit the changed behavior
+            [HarmonyPatch(typeof(Alert_HunterLacksRangedWeapon))]
+            [HarmonyPatch(MethodType.Constructor)]
+            internal static class Patch_HuntersLacksWeaponAlert
             {
-                ___defaultLabel = "HUMHunterLacksWeapon".Translate();
-                ___defaultExplanation = "HUMHunterLacksWeaponDesc".Translate();
+                static void Postfix(ref string ___defaultLabel, ref string ___defaultExplanation)
+                {
+                    ___defaultLabel = "HUMHunterLacksWeapon".Translate();
+                    ___defaultExplanation = "HUMHunterLacksWeaponDesc".Translate();
+                }
             }
         }
     }
--- Source/HuntersUseMelee/Properties/AssemblyInfo.cs
+++ Source/HuntersUseMelee/Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("HuntersUseMelee")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCulture("en")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 [assembly: NeutralResourcesLanguage("en")]
